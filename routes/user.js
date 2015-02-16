b415c17763d599c3f218ca39b748fd01adad4617
Christopher Banh <chrisngobanh@gmail.com>
2015-02-15 19:05:05 -0800

    Added re-captcha for account signups

--- routes/user.js
+++ routes/user.js
@@ -2,6 +2,7 @@
 module.exports = function(app) {
 
   'use strict';
+  var https = require('https');
   var validator = require('validator');
 
   var User = require('.././models/user');
@@ -17,6 +18,28 @@ module.exports = function(app) {
     var regex = /^[a-zA-Z ]+$/;
     return regex.test(str);
   };
+
+  var SECRET = process.env.CAPTCHA_SECRET ||
+              '6Lc4KAETAAAAAPAytG014cMHnx_89YCWk-269q4s';
+
+  function verifyRecaptcha(key, callback) {
+    https.get('https://www.google.com/recaptcha/api/siteverify?secret=' +
+      SECRET + '&response=' + key, function(res) {
+        var data = '';
+        res.on('data', function (chunk) {
+                data += chunk.toString();
+        });
+        res.on('end', function() {
+          try {
+            var parsedData = JSON.parse(data);
+            callback(parsedData.success);
+          } catch (e) {
+            callback(false);
+          }
+        });
+      }
+    );
+  }
 
   var handleLoginRequest = function(req, res) {
 
@@ -75,97 +98,114 @@ module.exports = function(app) {
 
   var handleSignupRequest = function(req, res) {
 
-    var username  = validator.toString(req.body.username);
-    var email     = validator.toString(req.body.email);
-    var password  = validator.toString(req.body.password);
-    var fName     = validator.toString(req.body.fName);
+    // Captcha key
+    var key = req.body['g-recaptcha-response'];
 
-    var result;
+    verifyRecaptcha(key, function(success) {
+      if (!success) {
 
-    if (req.session.user) {
-      result = {
-        code    : 400,
-        message : 'You already have an account.'
-      };
-      res.send(result);
-      return;
-    }
+        result = {
+          code    : 400,
+          message : 'Captcha failed. Try again.'
+        };
+        res.send(result);
+        return;
 
-    // Check to see if the user input a username that's between 4-20 characters
-    if (!validator.isLength(username, 4, 20)) {
-      result = {
-        code    : 400,
-        message : 'Username must be between 4-20 characters.'
-      };
-      res.send(result);
-      return;
-    }
-
-    if (!isUsername(username)) {
-      result = {
-        code    : 400,
-        message : 'Username can only contain letters, numbers, ' +
-                  'dashes, and underscores.'
-      };
-      res.send(result);
-      return;
-    }
-
-    if (!validator.isEmail(email)) {
-      result = {
-        code    : 400,
-        message : 'That is not a valid email.'
-      };
-      res.send(result);
-      return;
-    }
-
-    if (!password) {
-      result = {
-        code    : 400,
-        message : 'Password field cannot be empty.'
-      };
-      res.send(result);
-      return;
-    }
-
-    if (!fName) {
-      result = {
-        code    : 400,
-        message : 'First Name field cannot be empty.'
-      };
-      res.send(result);
-      return;
-    }
-
-    if (!isFullName(fName)) {
-      result = {
-        code    : 400,
-        message : 'Full Name can only contain letters and spaces.'
-      };
-      res.send(result);
-      return;
-    }
-
-    User.create(username, email, password, fName, function(err, doc) {
-      if (err) {
-        res.send(err);
       } else {
 
-        req.session.user = doc;
+        var username  = validator.toString(req.body.username);
+        var email     = validator.toString(req.body.email);
+        var password  = validator.toString(req.body.password);
+        var fName     = validator.toString(req.body.fName);
 
-        // 1 week
-        var week = 1000 * 60 * 60 * 24 * 7;
+        var result;
 
-        // Set the session to expire in a week
-        req.session.cookie.expires = new Date(Date.now() + week);
+        if (req.session.user) {
+          result = {
+            code    : 400,
+            message : 'You already have an account.'
+          };
+          res.send(result);
+          return;
+        }
 
-        res.send({
-          code    : 200,
-          message : 'Account successfully created.'
+        // Check to see if username is between 4-20 characters
+        if (!validator.isLength(username, 4, 20)) {
+          result = {
+            code    : 400,
+            message : 'Username must be between 4-20 characters.'
+          };
+          res.send(result);
+          return;
+        }
+
+        if (!isUsername(username)) {
+          result = {
+            code    : 400,
+            message : 'Username can only contain letters, numbers, ' +
+                      'dashes, and underscores.'
+          };
+          res.send(result);
+          return;
+        }
+
+        if (!validator.isEmail(email)) {
+          result = {
+            code    : 400,
+            message : 'That is not a valid email.'
+          };
+          res.send(result);
+          return;
+        }
+
+        if (!password) {
+          result = {
+            code    : 400,
+            message : 'Password field cannot be empty.'
+          };
+          res.send(result);
+          return;
+        }
+
+        if (!fName) {
+          result = {
+            code    : 400,
+            message : 'First Name field cannot be empty.'
+          };
+          res.send(result);
+          return;
+        }
+
+        if (!isFullName(fName)) {
+          result = {
+            code    : 400,
+            message : 'Full Name can only contain letters and spaces.'
+          };
+          res.send(result);
+          return;
+        }
+
+        User.create(username, email, password, fName, function(err, doc) {
+          if (err) {
+            res.send(err);
+          } else {
+
+            req.session.user = doc;
+
+            // 1 week
+            var week = 1000 * 60 * 60 * 24 * 7;
+
+            // Set the session to expire in a week
+            req.session.cookie.expires = new Date(Date.now() + week);
+
+            res.send({
+              code    : 200,
+              message : 'Account successfully created.'
+            });
+          }
+
         });
-      }
-
+    }
     });
   };
 
