99ce451cf557f13cf0efffce8dca839501b670fd
itstehkman <itstehkman@gmail.com>
2015-03-04 14:06:22 -0800

    added membersList.html and getAllMembers

--- routes/forum.js
+++ routes/forum.js
@@ -266,5 +266,5 @@ module.exports = function(app) {
   app.post('/makethread'      , handleThreadCreate);
   app.get('/thread/:id'       , handleThreadFetch);
   app.post('/thread/:id/reply', handleThreadReply);
-
+  
 };
